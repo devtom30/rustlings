bef7bcf702a5957c8d30cac6068a931da91b4e63
x2030280 <thomas.lornet@ratpsmartsystems.com>
2024-05-23 10:50:00 +0200

    some exercises done!

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -7,7 +7,7 @@
 // Execute `rustlings hint errors3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::num::ParseIntError;
 
@@ -15,13 +15,18 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost_result = total_cost(pretend_user_input);
 
-    if cost > tokens {
-        println!("You can't afford that many!");
+    if cost_result.is_err() {
+        println!("{}", cost_result.unwrap_err().to_string())
     } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+        let cost = cost_result.unwrap();
+        if cost > tokens {
+            println!("You can't afford that many!");
+        } else {
+            tokens -= cost;
+            println!("You now have {} tokens.", tokens);
+        }
     }
 }
 
